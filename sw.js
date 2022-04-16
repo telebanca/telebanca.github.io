b4ab101da881eb9de89b1291d22a5e32f07f249c
Jose Ernesto Carreno Bueno <josernesto1989@gmail.com>
2022-04-16 12:01:03 -0400

    cambiando sw

--- sw.js
+++ sw.js
@@ -37,3 +37,15 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+        if(key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
